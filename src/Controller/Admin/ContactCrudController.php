9e3cf9901e2759399688593a4b0ec9f3a2045842
Lucian <145598678+Dopapipo@users.noreply.github.com>
2024-04-18 19:20:48 +0200

    fix admin contact crud actions

--- src/Controller/Admin/ContactCrudController.php
+++ src/Controller/Admin/ContactCrudController.php
@@ -3,6 +3,7 @@
 namespace App\Controller\Admin;
 
 use App\Entity\Contact;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Actions;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
@@ -25,4 +26,8 @@ class ContactCrudController extends AbstractCrudController
         ];
     }
     */
+    public function configureActions(Actions $actions): Actions
+    {
+        return $actions->disable('new', 'edit');
+    }
 }
